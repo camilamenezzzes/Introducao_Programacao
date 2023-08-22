2e5b282e76ebfff51912e5b22f63537bee909c6e
Camila Menezes <camila.menezes@cest.edu.br>
2023-08-21 21:25:45 -0300

    Create Maior de 5.py

---
+++ Maior de 5.py
@@ -0,0 +1,5 @@
+lista = []
+n1, n2, n3, n4, n5 = str(input("Digite cinco valores: ")).split() # split serve para receber varios itens na mesma entrada
+lista.extend([n1,n2,n3,n4,n5]) # extend serve para adicionar muitas variaveis na mesma lista
+lista.sort() # sort serve para ordenar crescentemente a lista
+print("O maior número da lista é: ", lista[4])
